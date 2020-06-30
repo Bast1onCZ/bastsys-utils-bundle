4289d2c0f9e7c36ec7fa4b7174779d9e58deb286
klobaska <mir.klobaska@seznam.cz>
2020-06-30 12:25:32 +0200

    implemented NotImplementedException.php

---
+++ Exception/NotImplementedException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BastSys\UtilsBundle\Exception;
+
+use Throwable;
+
+/**
+ * Class NotImplementedException
+ * @package BastSys\UtilsBundle\Exception
+ * @author mirkl
+ */
+class NotImplementedException extends \Exception
+{
+    /**
+     * NotImplementedException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = 'This piece of code was not implemented', $code = 501, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
