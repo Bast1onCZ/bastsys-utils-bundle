49e7b54fb09b29eb72566de09671daf1b7d89916
klobaska <mir.klobaska@seznam.cz>
2020-08-06 11:48:18 +0200

    removed EntityException.php and changed EntityNotFoundException.php to extend from RuntimeException

--- Exception/Entity/EntityNotFoundException.php
+++ Exception/Entity/EntityNotFoundException.php
@@ -3,12 +3,23 @@ declare(strict_types=1);
 
 namespace BastSys\UtilsBundle\Exception\Entity;
 
+use Throwable;
+
 /**
  * Class EntityNotFoundException
  * @package BastSys\UtilsBundle\Exception\Entity
  * @author mirkl
  */
-class EntityNotFoundException extends EntityException
+class EntityNotFoundException extends \RuntimeException
 {
-
+    /**
+     * EntityNotFoundException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "", $code = 404, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
