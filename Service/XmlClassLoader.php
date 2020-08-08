48acc9a67495a05406c9a2ec994b7f64ca0c2c84
klobaska <mir.klobaska@seznam.cz>
2020-08-08 16:49:05 +0200

    implemented findOneBy in AEntityRepository.php

---
+++ Service/XmlClassLoader.php
@@ -0,0 +1,124 @@
+<?php
+declare(strict_types=1);
+
+namespace BastSys\UtilsBundle\Service;
+
+use BastSys\UtilsBundle\Exception\NotImplementedException;
+
+/**
+ * Class XmlClassLoader
+ * @package BastSys\UtilsBundle\Service
+ * @author mirkl
+ *
+ * @todo: test this class
+ */
+class XmlClassLoader
+{
+    /**
+     * @param string $class
+     * @param \SimpleXMLElement $data
+     *
+     * @return object instance of given class
+     * @throws \ReflectionException
+     * @throws NotImplementedException
+     */
+    public function loadClass(string $class, \SimpleXMLElement $data): object {
+        $instance = new $class();
+
+        $ref = new \ReflectionClass($class);
+        $properties = $this->getAllProperties($ref);
+
+        foreach($properties as $prop) {
+            $propName = $prop->getName();
+            $isAccessible = $prop->isPublic();
+
+            if(!$isAccessible) {
+                $prop->setAccessible(true);
+            }
+
+            $type = $this->getDocVarType($prop);
+            $value = $this->typeConvert($data->$propName ?? '', $type);
+            $prop->setValue($instance, $value);
+
+            if(!$isAccessible) {
+                $prop->setAccessible(false);
+            }
+        }
+
+        return $instance;
+    }
+
+    /**
+     * @param \ReflectionClass $class
+     * @return \ReflectionProperty[]
+     */
+    private function getAllProperties(\ReflectionClass $class): array {
+        $filter = \ReflectionProperty::IS_PRIVATE | \ReflectionProperty::IS_PROTECTED | \ReflectionProperty::IS_PUBLIC;
+        $properties = $class->getProperties($filter);
+
+        $parentClass = $class;
+        while($parentClass = $parentClass->getParentClass()) {
+            $parentProperties = $parentClass->getProperties($filter);
+            foreach($parentProperties as $parentProperty) {
+                $properties[] = $parentProperty;
+            }
+        }
+
+        return $properties;
+    }
+
+    /**
+     * @param \ReflectionProperty $property
+     * @return string
+     */
+    private function getDocVarType(\ReflectionProperty $property): string {
+        $matches = [];
+        preg_match('/@var\s+([\S]+)/', $property->getDocComment(), $matches);
+        if(!isset($matches[1])) {
+            $propName = $property->getName();
+            throw new \InvalidArgumentException("Doc comment of property '$propName' does not contain valid @var annotation");
+        }
+
+        return $matches[1];
+    }
+
+    /**
+     * @param string $value
+     * @param \ReflectionType $type
+     * @return \DateTime|string
+     * @throws NotImplementedException
+     */
+    private function typeConvert(string $value, string $phpdocType) {
+        $result = $value;
+
+        $types = explode('|', $phpdocType);
+
+        if($value === null && in_array('null', $types)) {
+            return null;
+        }
+
+        $preferredType = $types[0];
+
+        switch($preferredType) {
+            case 'string':
+            case 'int':
+            case 'float':
+            case 'bool':
+            case 'boolean':
+                settype($result, $preferredType);
+                break;
+            case 'DateTime':
+            case '\DateTime':
+                try {
+                    $result = new \DateTime($value);
+                } catch(\Exception $ex) {
+                    throw new \InvalidArgumentException("Invalid datetime '$preferredType'");
+                }
+                break;
+            default:
+                throw new NotImplementedException("Type '$preferredType' not implemented");
+        }
+
+        return $result;
+    }
+}
