c6b295cc18c2c29bb666d2aafc85d60f19bcbb58
klobaska <mir.klobaska@seznam.cz>
2020-08-09 19:48:25 +0200

    implemented CleanXmlElement.php

---
+++ Model/CleanXmlElement.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace BastSys\UtilsBundle\Model;
+
+/**
+ * Class CleanXmlElement
+ * @package BastSys\UtilsBundle\Model
+ * @author mirkl
+ */
+class CleanXmlElement extends \SimpleXMLElement
+{
+    /**
+     * Returns xml as string and clears all comments
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        $result = parent::asXML();
+
+        return preg_replace('/<\?.+\?>/', '', $result);
+    }
+}
